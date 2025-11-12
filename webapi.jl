8fbdf42bac16ffaf6976a2670e02b4d4a55daa94
Maritza Arango <Maritzaaarangog@gmail.com>
2025-11-11 22:09:11 -0600

    actualizando el webapi - PEER PROGRAMMING

--- webapi.jl
+++ webapi.jl
@@ -4,12 +4,15 @@ using UUIDs
 
 route("/run") do
     run!(model, 1)
+    
+    # Note: Banana maintenance is handled automatically in model_step!
+    
     agents = []
     for ghost in allagents(model)
         push!(agents, ghost)
     end
 
-    json(Dict(:msg => "Adios", "agents" => agents, "bananas" => collect(banana_positions)))
+    json(Dict(:msg => "Step completed", "agents" => agents, "bananas" => collect(banana_positions)))
 end
 
 route("/new-banana") do
@@ -25,10 +28,42 @@ route("/bananas") do
     json(Dict(:msg => "Current bananas", "bananas" => collect(banana_positions)))
 end
 
+route("/regenerate-bananas") do
+    # Clear existing bananas and regenerate
+    empty!(banana_positions)
+    for i in 1:num_bananas_respawn
+        place_banana_randomly()
+    end
+    json(Dict(:msg => "Bananas regenerated for study purposes", "bananas" => collect(banana_positions)))
+end
+
+route("/game-state") do
+    # Collect all agent positions
+    mice_positions = []
+    cat_position = nothing
+    
+    for agent in allagents(model)
+        if agent isa Ghost
+            push!(mice_positions, Dict("id" => agent.id, "pos" => [agent.pos[1], agent.pos[2]], "state" => string(agent.state)))
+        elseif agent isa Gato
+            cat_position = Dict("id" => agent.id, "pos" => [agent.pos[1], agent.pos[2]], "state" => string(agent.state))
+        end
+    end
+    
+    json(Dict(
+        :msg => "Current game state",
+        "mice" => mice_positions,
+        "cat" => cat_position,
+        "bananas" => collect(banana_positions),
+        "grid_size" => [size(matrix, 1), size(matrix, 2)]
+    ))
+end
+
 Genie.config.run_as_server = true
 Genie.config.cors_headers["Access-Control-Allow-Origin"] = "*"
 Genie.config.cors_headers["Access-Control-Allow-Headers"] = "Content-Type"
 Genie.config.cors_headers["Access-Control-Allow-Methods"] = "GET,POST,PUT,DELETE,OPTIONS"
 Genie.config.cors_allowed_origins = ["*"]
 
+#up(host="0.0.0.0", port=6000)
 up()
